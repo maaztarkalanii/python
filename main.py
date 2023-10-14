abd1d185863bc6f8f7e19613d39876359dd10351
Maaz Ali <Maaztarkalanii@gmail.com>
2023-10-14 23:32:41 +0500

    this is new commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#in this programm we are going to be work with some of the variable and data types 
+#Data types in programming represent the kind of data that a variable can hold.
+# Data types determine what operations can be performed on the data,
+# how much memory is allocated to store the data, and how the data is represented internally. 
+
+a =10
+b=20
+sum = a+b
+print(sum)
+# this is how we can use a int value in python
+# now it will return a int value 30 
+
+a =10.5
+b=20.6
+sum = a+b
+print(sum)
+# here we can see this clearly that we can save two variables with same name but different data types 
+
+# now we are moving toward strings
+name = "Maaz Ali"
+print(name)
+  
+
